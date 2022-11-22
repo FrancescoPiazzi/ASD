771e9c24238b77e237b6b1774bba68209de9e465
= <=>
2022-11-22 08:20:27 +0100

    aggiunto cout di righe e colonne

--- submissions/matrice-somma-max/src/generate-input.cpp
+++ submissions/matrice-somma-max/src/generate-input.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-const int ROWS = 3;
-const int COLS = 7;
+const int ROWS = 500;
+const int COLS = 500;
 
 using namespace std;
 
 int main(){
     srand(time(NULL));
+    cout << ROWS << " " << COLS << endl;
     for(int i=0; i<ROWS; i++){
         for(int j=0; j<COLS; j++){
             cout << rand()%100-60 << " ";
