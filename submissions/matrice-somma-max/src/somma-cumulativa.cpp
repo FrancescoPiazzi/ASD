32799e22dcef20db49a4aedaef53def6b025a426
= <=>
2022-11-22 08:46:46 +0100

    chi ha bisogno dei print? o delle graffe?

--- submissions/matrice-somma-max/src/somma-cumulativa.cpp
+++ submissions/matrice-somma-max/src/somma-cumulativa.cpp
@@ -10,12 +10,7 @@ using namespace std;
 
 const int SIZE = 2000;
 
-void printMatrix(int **m, int, int);
-void printMatrix(int **m, int, int, int, int);
-void printVector(int v[], int);
-
 int maxMatrix(int **a, int, int);
-int maxVect(int a[], int);
 int** cumulativeMatrix(int **a, int, int);
 int getSum(int **m, int startX, int startY, int endX, int endY);
 
@@ -32,17 +27,13 @@ int main(){
     f >> r >> c;
     for(int i=0; i<r*c; i++)
         f >> a[i/c][i%c];
-
     f.close();
 
     int **cumulative = cumulativeMatrix(a, r, c);
-
     mx = maxMatrix(cumulative, r, c);
     g << mx;
 
-    
     g.close();
-
     return 0;
 }
 
@@ -50,25 +41,19 @@ int main(){
 int maxMatrix(int **m, int r, int c){
     int mx=0;
 
-    for(int startX=0; startX<r; startX++){
-        for(int startY=0; startY<c; startY++){
-            for(int endX=startX; endX<r; endX++){
-                for(int endY=startY; endY<c; endY++){
+    // chi ha bisogno delle graffe?
+    for(int startX=0; startX<r; startX++)
+        for(int startY=0; startY<c; startY++)
+            for(int endX=startX; endX<r; endX++)
+                for(int endY=startY; endY<c; endY++)
                     mx = max(mx, getSum(m, startX, startY, endX, endY));
-                    // cout << startX << "," << startY << "  " << endX << "," << endY << " -> " << getSum(m, startX, startY, endX, endY) << endl;
-                }
-            }
-        }
-    }
-
-   // cout << getSum(m, 0, 1, 1, 2) << endl;
 
     return mx;
 }
 
-int getSum(int **m, int startX, int startY, int endX, int endY){
-    // data una matrice di somme cumulative m, trova la somma della sottomatrice
 
+// data una matrice di somme cumulative m, trova la somma della sottomatrice
+int getSum(int **m, int startX, int startY, int endX, int endY){
     // se start ed end sono a zero non devo sottrarre nulla
     if(startX == 0 && startY == 0)
         return m[endX][endY];
@@ -80,12 +65,11 @@ int getSum(int **m, int startX, int startY, int endX, int endY){
         return m[endX][endY] - m[startX-1][endY];
 
     // altrimenti mi tocca fare dei conti
-    return m[endX][endY] - 
-        m[startX-1][endY] - 
-        m[endX][startY-1] + 
-        m[startX-1][startY-1];
+    return m[endX][endY] - m[startX-1][endY] - m[endX][startY-1] + m[startX-1][startY-1];
 }
 
+
+// calcola la matrice di somma cumulativa
 int** cumulativeMatrix(int **a, int r, int c){
     int** b = new int*[SIZE];
     for(int i=0; i<SIZE; i++)
@@ -99,42 +83,9 @@ int** cumulativeMatrix(int **a, int r, int c){
     }
 
     // il resto della matrice
-    for(int i=1; i<r; i++){
-        for(int j=1; j<c; j++){
+    for(int i=1; i<r; i++)
+        for(int j=1; j<c; j++)
             b[i][j] = a[i][j] + b[i-1][j] + b[i][j-1] - b[i-1][j-1];
-        }
-    }
 
     return b;
-}
-
-int maxVect(int a[], int size){
-    int max1=0, max2=0;
-    for(int i=0; i<size; i++){
-        max1 = max(0, max1+a[i]);
-        max2 = max(max1, max2);
-    }
-    return max2;
-}
-
-
-void printVector(int v[], int size){
-    for(int i=0; i<size; i++)
-        cout << v[i] << " ";
-    cout << endl;
-}
-
-
-void printMatrix(int **m, int endX, int endY){
-    printMatrix(m, 0, 0, endX, endY);
-}
-
-
-void printMatrix(int **m, int startX, int startY, int endX, int endY){
-    for(int i=startX; i<endX; i++){
-        for(int j=startY; j<endY; j++){
-            cout << m[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
